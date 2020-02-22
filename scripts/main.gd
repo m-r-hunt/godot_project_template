d1d992fb9214f3df81d9c3cacd5313b171aec586
Max Hunt <max@huntw3.com>
2020-02-22 16:22:27 +0000

    Make main state machine more extensible

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,22 +4,32 @@ extends Node2D
 var current_node
 
 
+var state_machine = {
+	"title": ["res://scenes/screens/title.tscn", ["start_playing", "on_start_playing"], ["quit", "on_quit"], ["credits", "on_credits"]],
+	"gameplay": ["res://scenes/screens/gameplay.tscn", ["finished_game", "on_finished_game"], ["back_to_title", "on_back_to_title"]],
+	"credits": ["res://scenes/screens/credits.tscn", ["return_to_title", "on_return_to_title"]],
+	"game_complete": ["res://scenes/screens/game_complete.tscn", ["credits", "on_credits"]],
+}
+
+
 func _ready():
-	load_title_screen()
+	load_state("title")
 
 
-func load_title_screen():
-	var ts = load("res://scenes/screens/title.tscn").instance()
-	current_node = ts
-	add_child(current_node)
-	Utils.e_connect(ts, "start_playing", self, "on_start_playing")
-	Utils.e_connect(ts, "quit", self, "on_quit")
-	Utils.e_connect(ts, "credits", self, "on_credits")
+func load_state(s):
+	if current_node:
+		current_node.queue_free()
+		current_node = null
+	var state = state_machine[s]
+	var node = load(state[0]).instance()
+	add_child(node)
+	for i in range(1, len(state)):
+		Utils.e_connect(node, state[i][0], self, state[i][1])
+	current_node = node
 
 
 func on_start_playing():
-	current_node.queue_free()
-	load_gameplay()
+	load_state("gameplay")
 
 
 func on_quit():
@@ -28,45 +38,19 @@ func on_quit():
 		OS.window_fullscreen = false
 
 
-func load_gameplay():
-	var gp = load("res://scenes/screens/gameplay.tscn").instance()
-	current_node = gp
-	add_child(current_node)
-	Utils.e_connect(gp, "finished_game", self, "on_finished_game")
-	Utils.e_connect(gp, "back_to_title", self, "on_back_to_title")
-
-
 func on_back_to_title():
-	current_node.queue_free()
 	get_tree().paused = false
-	load_title_screen()
+	load_state("title")
 
 
 func on_finished_game():
-	current_node.queue_free()
 	get_tree().paused = false
-	load_game_complete()
-
-
-func load_game_complete():
-	var gc = load("res://scenes/screens/game_complete.tscn").instance()
-	current_node = gc
-	add_child(current_node)
-	Utils.e_connect(gc, "credits", self, "on_credits")
+	load_state("game_complete")
 
 
 func on_credits():
-	current_node.queue_free()
-	load_credits()
-
-
-func load_credits():
-	var c = load("res://scenes/screens/credits.tscn").instance()
-	current_node = c
-	add_child(current_node)
-	Utils.e_connect(c, "return_to_title", self, "on_return_to_title")
+	load_state("credits")
 
 
 func on_return_to_title():
-	current_node.queue_free()
-	load_title_screen()
+	load_state("title")
