18e8347ff605d44084c66793380663a8d036a4a6
Max Hunt <max@huntw3.com>
2020-03-08 18:01:51 +0000

    Make button scene into a 2d node by default

--- scripts/button.gd
+++ scripts/button.gd
@@ -1,4 +1,25 @@
-extends Button
+extends Node2D
+
+tool
+
+
+export var text := "" setget set_text
+
+
+signal pressed
+
+
+func _ready():
+	Utils.e_connect($Button, "pressed", self, "on_pressed")
+
+
+func set_text(t):
+	text = t
+	$Button.text = t
+
+
+func on_pressed():
+	emit_signal("pressed")
 
 
 func set_selected(_selected: bool):
