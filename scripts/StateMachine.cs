817d5a7aea16ea79215ba828eb33202c5ebbad6a
Maximilian Hunt <max@huntw3.com>
2021-03-19 17:37:03 +0000

    Start working on state machine code

---
+++ scripts/StateMachine.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+public class StateMachine<T>
+{
+    public abstract class IState
+    {
+        public Dictionary<string, string> Signals;
+
+        public event Action<string> SignalEmitted;
+
+        protected void Emit(string signal)
+        {
+            SignalEmitted?.Invoke(signal);
+        }
+
+        public virtual void Enter(T self)
+        {
+            
+        }
+
+        public virtual void Exit(T self)
+        {
+            
+        }
+        
+        public abstract void Process(T self, float dt);
+    }
+
+    private readonly Dictionary<string, IState> _states = new Dictionary<string, IState>();
+
+
+    public string CurrentState { get; private set; }
+    private readonly T _owner;
+
+    public StateMachine(T owner, string current)
+    {
+        foreach (var t in typeof(T).GetNestedTypes(BindingFlags.NonPublic))
+        {
+            if (typeof(IState).IsAssignableFrom(t))
+            {
+                _states[t.Name] = (IState)Activator.CreateInstance(t);
+                _states[t.Name].SignalEmitted += Emit;
+            }
+        }
+        
+        _owner = owner;
+        CurrentState = current;
+    }
+
+    private void Emit(string signal)
+    {
+        var c = _states[CurrentState];
+        CurrentState = c.Signals[signal];
+    }
+
+    public void Process(float dt)
+    {
+        _states[CurrentState].Process(_owner, dt);
+    }
+}
