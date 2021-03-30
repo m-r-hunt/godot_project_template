b9a9cd2b7ee3084e69b484d27b7fe58ad5ae027e
Maximilian Hunt <max@huntw3.com>
2021-03-30 16:26:40 +0100

    Improve plugins slightly

--- addons/aseprite_import/AsepriteSprite.cs
+++ addons/aseprite_import/AsepriteSprite.cs
@@ -3,19 +3,23 @@ using System;
 
 public class AsepriteSprite : Sprite
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    private AnimationPlayer _player;
 
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        _player = GetNode<AnimationPlayer>("AnimationPlayer");
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public void Play(string name)
+    {
+        _player.Play(name);
+    }
+
+    public void PlayIfNew(string name)
+    {
+        if (_player.CurrentAnimation != name)
+        {
+            _player.Play(name);
+        }
+    }
 }
